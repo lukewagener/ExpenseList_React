9bd413c9c83fe58f926ea423a97111de787d31c9
lukewagener <lwagener@aceproject.space>
2023-05-04 13:04:10 -0500

    Added click event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,13 +3,16 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 export default function ExpenseItem(props) {
-  
+  const clickHandler = () => {
+    console.log("Clicked!!! SUSSSYYYYYY");
+  };
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
